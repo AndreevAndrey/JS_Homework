e9267440daaaa7172ce9c05af482f39738a9421d
AndreevAndrey <888Andrei888@mail.ru>
2019-08-22 08:50:03 +0300

    Homework 17

--- Lesson-17/HW_17.js
+++ Lesson-17/HW_17.js
@@ -1,24 +1,27 @@
-var startButton = document.getElementById('start');
-var reset = document.getElementById('reset');
-var result = document.getElementById('result');
+var startButton = document.getElementById('start'),
+    reset = document.getElementById('reset'),
+    result = document.getElementById('result'),
+    x;
 reset.className = 'hide';
 result.className = 'hide';
-var x;
 startButton.dataset.state = 'Stop';
 
 startButton.onclick = function () {
     reset.className = 'active';
     result.className = 'active';
-    if (startButton.dataset.state === 'Stop') {
-        start();
-        startButton.innerHTML = 'Stop';
-        startButton.style.backgroundColor = 'red';
-        startButton.dataset.state = 'Start';
-    } else if (startButton.dataset.state === 'Start') {
-        startButton.innerHTML = 'Start';
-        startButton.style = '#start';
-        startButton.dataset.state = 'Stop';
-        stop();
+    switch (startButton.dataset.state) {
+        case 'Stop':
+            start();
+            startButton.innerHTML = 'Stop';
+            startButton.style.backgroundColor = 'red';
+            startButton.dataset.state = 'Start';
+            break;
+        case 'Start':
+            startButton.innerHTML = 'Start';
+            startButton.style = '#start';
+            startButton.dataset.state = 'Stop';
+            stop();
+            break;
     }
 };
 
@@ -30,13 +33,13 @@ function stop() {
     clearInterval(x);
 }
 
-var millisec = 0;
-var sec = 0;
-var min = 0;
-var milliSecOut = 0;
-var secOut = 0;
-var minOut = 0;
-var counter = 0;
+var millisec = 0,
+    sec = 0,
+    min = 0,
+    milliSecOut = 0,
+    secOut = 0,
+    minOut = 0,
+    counter = 0;
 
 function timer() {
     milliSecOut = checkTime(millisec);
@@ -53,6 +56,8 @@ function timer() {
     }
     if (min === 60) {
         stop();
+        result.className = 'hide';
+        startButton.className = 'hide';
     }
     document.getElementById('millisec').innerHTML = milliSecOut;
     document.getElementById('sec').innerHTML = secOut;
@@ -63,12 +68,7 @@ function timer() {
         counter++;
         document.getElementById('val').innerHTML += counter + ') ' + time + '<br>';
     };
-
     localStorage.setItem('time', time);
-    // localStorage.getItem('time');Дальше как при загрузке страницы применить эти данные - не понимаю...Window.onload
-    // внутри этой же функции не сработает,если поставить условие,что есть данные в localStorage - запускать и
-    // плюсовать их к запуску с 0. Если,как положено,в глобальной области прописывать window.onload- не видит данные
-    // ,которые нужны.В общем что-то упускаю
 }
 
 function checkTime(i) {
@@ -80,6 +80,7 @@ function checkTime(i) {
 
 reset.onclick = function () {
     stop();
+    startButton.className = 'active';
     startButton.dataset.state = 'Stop';
     startButton.innerHTML = 'Start';
     startButton.style = '#start';
